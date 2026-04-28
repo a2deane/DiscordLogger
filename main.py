c746b0527627f5eb5745f89aabe30ec85174a542
SirPaniniee <67246205+AidenDeane@users.noreply.github.com>
2026-04-28 15:37:50 -0400

    Add edit, reaction logging.

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ import time
 from config import token
 from config import directory
 
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
 
     async def on_message(self, message):
-
         if message.content == 'ping':
             await message.channel.send('pong')
 
@@ -25,5 +25,26 @@ class MyClient(discord.Client):
             file.write(f'{time.strftime('%d/%m/%Y - %H:%M',time.localtime())} {message.author} (ID = {message.author.id}):\n')
             file.write(f'\t{message.content}\n')
 
+    # Reaction Log (Only logs reactions to messages sent during runtime)
+    async def on_reaction_add(self, reaction, user):
+        with open(f'{reaction.message.channel}.txt','a') as file:
+            file.write(f'{time.strftime('%d/%m/%Y - %H:%M',time.localtime())} {user} (ID = {user.id}):\n')
+            file.write(f'\tReacted {reaction} to: {reaction.message.content}\n')
+
+    async def on_reaction_remove(self,reaction,user):
+        with open(f'{reaction.message.channel}.txt','a') as file:
+            file.write(f'{time.strftime('%d/%m/%Y - %H:%M',time.localtime())} {user} (ID = {user.id}):\n')
+            file.write(f'\tRemoved reaction {reaction} to: {reaction.message.content}\n')
+
+
+    # Edit Log
+
+    async def on_message_edit(self,before,after):
+        with open(f'{before.channel}.txt','a') as file:
+            file.write(f'{time.strftime('%d/%m/%Y - %H:%M',time.localtime())} {before.author} (ID = {before.author.id}):\n')
+            file.write(f'\tEdited {before.content} to {after.content}\n')
+ 
+
 client = MyClient(chunk_guilds_at_startup=False)
 client.run(token)
+
