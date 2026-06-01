369b17ad02dbf8e86c99587957750198140397b8
SirPaniniee <67246205+AidenDeane@users.noreply.github.com>
2026-06-01 17:51:34 -0400

    Overhaul s.t. CLI interface works

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='DiscordScraper',
-    version='1.0.22',
+    version='1.0.1',
     packages=find_packages(),
-    include_package_data=True,
     install_requires=[
         'discord.py-self[voice]>=2.1.0'
     ],
@@ -12,7 +11,8 @@ setup(
         'console_scripts': [
             'discord-logger=DiscordLogger.main:run_bot',
             'update-token=DiscordLogger.main:update_token',
-            'dl-dir=DiscordLogger.main:get_dir'
+            'dl-dir=DiscordLogger.main:get_dir',
+            'dl-setup=DiscordLogger.main:setup'
         ]
     },
     author='Aiden Deane',
